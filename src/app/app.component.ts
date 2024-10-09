5e12b2f2f9731bc743e76af202ca72a50375f1cd
Artem Ivanov <artem443344@gmail.com>
2024-10-09 19:47:00 +0300

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	new file:   src/app/list-ui/list/list.component.html 	new file:   src/app/list-ui/list/list.component.scss 	new file:   src/app/list-ui/list/list.component.spec.ts 	new file:   src/app/list-ui/list/list.component.ts 	modified:   src/styles.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list-ui/list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
