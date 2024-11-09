febdde422d041ab21da85f6e45418d42303a4331
Artem Ivanov <artem443344@gmail.com>
2024-11-09 16:06:25 +0300

    modified:   src/app/app.component.html 	modified:   src/app/app.component.scss 	modified:   src/app/app.component.ts 	modified:   src/app/list-ui/list/list.component.html 	modified:   src/app/list-ui/list/list.component.scss 	modified:   src/app/list-ui/list/list.component.ts 	new file:   src/assets/create_icon.png 	new file:   src/assets/delete_icon.png 	new file:   src/assets/edit_icon.png 	new file:   src/assets/save_btn.png

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListComponent } from './list-ui/list/list.component';
+import { CommonModule } from '@angular/common'; 
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListComponent],
+  imports: [RouterOutlet, ListComponent, CommonModule,],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'FirstProject';
+  cards: { isReadonly: boolean, isDeleted: boolean }[] = [];
+  
+  addCard() {
+    this.cards.push({ isReadonly: true, isDeleted: false });
+  }
 }
--- src/app/list-ui/list/list.component.ts
+++ src/app/list-ui/list/list.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  @Input() cards: { isReadonly: boolean, isDeleted: boolean }[] = [];
+  @Output() addCardEvent = new EventEmitter<void>();
 
+  offReadonly(index: number) {
+    this.cards[index].isReadonly = !this.cards[index].isReadonly;
+  }
+
+  onReadonly(index: number) {
+    this.cards[index].isReadonly = !this.cards[index].isReadonly;
+  }
+
+  deleteCard(index: number) {
+    this.cards.splice(index, 1);
+  }
 }
