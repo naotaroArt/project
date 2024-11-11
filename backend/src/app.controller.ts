f2500a5e08383b26c3c4210b11db2d93da478fe3
Artem Ivanov <artem443344@gmail.com>
2024-11-11 22:06:20 +0300

    modified:   backend/package-lock.json 	modified:   backend/package.json 	modified:   backend/src/app.controller.ts 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/app/app.component.scss 	modified:   package.json

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -4,6 +4,9 @@ import { ApiTags, ApiResponse } from '@nestjs/swagger';
 @Controller('api')
 @ApiTags('Example')
 export class AppController {
+  getHello(): any {
+    throw new Error('Method not implemented.');
+  }
   @Get('data')
   @ApiResponse({ status: 200, description: 'Data retrieved successfully.' })
   getData() {
